946653be01f4885921cff056f23d274a77e6f8c2
Drentorn <siliyumtr@gmail.com>
2020-11-07 10:00:58 +0300

    do rough result screen, clear code

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
 //make is submit by enter
 //window.addEventListener("keydown",commands(document.getElementById('txt1').value))
+let write=(text)=> {
+const x = document.createElement("li");                        // Create a <p> node
+const t = document.createTextNode(text);    // Create a text node
+x.appendChild(t);                                           // Append the text to <p>
+document.getElementById("text-field").appendChild(x);                   
+}
 
 const openLeft=()=> document.getElementById("leftBar").style.width = "25%";
 const  closeLeft=()=> document.getElementById("leftBar").style.width = "0";
 swap1=()=> {
     document.getElementById("friend").style.visibility = "hidden";
-    document.getElementById("play").style.visibility = "visible";   };
+    document.getElementById("play").style.visibility = "visible";   
+};
 
-    let comm = document.getElementById('filename')
-
-    let commandString = ""
-    let inventory ={
-        medicine:10
-
-    }
-    let = items ={
-        rope: 10,
-        net: 10,
-        medicine:10,
-    }    
     let eatJokes = ["hai", " no","find","don't eat"] 
-
-    let takeItem = (item) =>{
-        currentLocation.take 
-
-    }
     harbor ={
         look :"Nice place",
         north:"city",
@@ -42,60 +32,53 @@ swap1=()=> {
     }
 
     let commands=(command)=> { 
+        document.getElementById("txt1").value = ""
     
         switch (command) {
             case"look": case "where":
-                console.log(currentLocation.look);
-                console.log(currentLocation.take);
+                write(currentLocation.look);
+                write(currentLocation.take);
                     break;
             case "west":case "w":
              currentLocation.west
-             console.log(currentLocation.look);
+             write(currentLocation.look);
              break;
             case "east": case "e":
                 currentLocation = currentLocation.east
-                console.log(currentLocation.look);
+                write(currentLocation.look);
 
                 break;
             case "south":case "s":
                 currentLocation = currentLocation.south
-                console.log(currentLocation.look);
+                write(currentLocation.look);
 
                 break;
             case "north" :case "n":
                 currentLocation = currentLocation.north
-                console.log(currentLocation.look);
+                write(currentLocation.look);
 
                 break;
             case "take" :case "pick up" :case "pick":
-                commandString += "take"
-                console.log("take what?")
-                //comm
-                if (currentLocation.take == 0) { console.log("haii")}
-                else{ takeItem(currentLocation.take);
-                }break;
+               
+                break;
             case "eat $(x)" :
-                    commandString += "eat";
-                    console.log("eat what ?");
-                    //take spesifier object from player
-                    //code here
-                    //--------------------------------
-
-                console.log(eatJokes[Math.floor(Math.random()*4) ]);
-                    commandString = ""
+            write(eatJokes[Math.floor(Math.random()*4) ]);
                 break;
             default:
-                console.log("listen here you lil")
+                write("Sorry I don't understand")
+               
                 break;
         }
      
 };
 
-let currentLocation = harbor;
 
+
+
+let currentLocation = harbor;
 
 harbor.north = city
 harbor.east = ocean
 harbor.south = home
 harbor.west = land
-//remove the case statements
+//remove the case statements maybe ?
